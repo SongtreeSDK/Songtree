36e3385400019093c346250379a093777709b284
Andrea <andrea@MacBook-Pro-di-Andrea.local>
2015-09-24 11:16:09 +0200

    Remove build directory

--- build/SongtreeSDKTest.build/Debug-iphoneos/SongtreeSDKTest.build/Objects-normal/armv7/main.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/andrea/nTrack/trunk/n-Track_EX/iOS/SongtreeSDKTest/Example/SongtreeSDKTest/main.m \
-  /Users/andrea/nTrack/trunk/n-Track_EX/iOS/SongtreeSDKTest/Example/SongtreeSDKTest/AppDelegate.h
--- build/SongtreeSDKTest.build/Debug-iphonesimulator/SongtreeSDKTest.build/Script-72C5F65A1B84DC11005CAC4D.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo "Deleting static lib in SongtreeSDK framework:"
-THEPATH=${TARGET_BUILD_DIR}/${PRODUCT_NAME}.app/SongtreeSDK.framework/SongtreeSDK
-echo -n ${THEPATH}
-if [ -f ${THEPATH} ] ; then
-rm ${THEPATH}
-fi
